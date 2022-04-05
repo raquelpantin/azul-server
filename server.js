adb032bb5550c63ab6546a655a419c215736bfca
Raquel Pantin <raquel.pantin@gmail.com>
2022-04-05 18:07:23 -0400

    cleaned up unused code

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const port = process.env.PORT || 8000;
 
 const app = express();
 
+//connect to mongodb
 connectDB();
 
 //middleware
@@ -18,6 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
+//routes
 app.use("/artist", artistRoutes);
 app.use("/user", userRoutes);
 
